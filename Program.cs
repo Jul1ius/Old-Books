89c8980f2354e3b723e9f8d961290ede7529eb77
its julii <nanji828@gmail.com>
2022-12-15 20:41:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            int counter = 0;
+            bool isBookFound = false;
+            string nextBooks = Console.ReadLine();
+
+            while (nextBooks != "No More Books")
+            {              
+                if (favouriteBook == nextBooks)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                counter++;
+                nextBooks = Console.ReadLine();
+            }
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+
+            }
+        }
+    }
+}
